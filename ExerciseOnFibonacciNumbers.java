d663616fa6bbe728a528781e42a37cdbe264fe86
Oleksandr Semochko <semochko@gmail.com>
2016-03-26 18:28:48 +0200

    Add final program code

--- ExerciseOnFibonacciNumbers.java
+++ ExerciseOnFibonacciNumbers.java
@@ -3,17 +3,32 @@ package edu.cursor.mavenHomework.service;
 import java.util.Scanner;
 /**
  * This class contains the method(s) used to solve the problem on Fibonacci numbers.
- * @author 
+ * @author Semochko Oleksandr
  *
  */
 
 public class ExerciseOnFibonacciNumbers {
 	
 	public int fibonacci(){
-		Scanner sc = new Scanner(System.in);
-		
-		return 0;
-		
-	}
+	private static int f(int index) {
+        if (index <= 0) {
+            return 0;
+        } else if (index == 1) {
+            return 1;
+        } else if (index == 2) {
+            return 1;
+        } else {
+            return f(index - 1) + f(index - 2);
+        }
+    }
 
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Enter amount of Fibonacci numbers to show:\n");
+		int n = sc.nextInt();
+    	
+        for (int i = 1; i <= n; i++) {
+            System.out.print(f(i) + " ");
+        }
+    }
 }
